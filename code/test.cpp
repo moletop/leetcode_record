a7ab8e6591aa82ff9ff8d5a3c7e39dc0c366bd9b
molotop <liuyixd@163.com>
2021-05-24 22:22:55 +0800

    24th may

--- code/test.cpp
+++ code/test.cpp
@@ -1,49 +1,142 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <map>
-#include <unordered_set>
-#include <deque>
+#include <algorithm>
 using namespace std;
 
-class Solution {
-public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n = nums.size();
-        deque<int> q;
-        for (int i = 0; i < k; ++i) {
-            while (!q.empty() && nums[i] >= nums[q.back()]) {
-                q.pop_back();
-            }
-            q.push_back(i);
-        }
-
-        vector<int> ans = {nums[q.front()]};
-        for (int i = k; i < n; ++i) {
-            while (!q.empty() && nums[i] >= nums[q.back()]) {
-                q.pop_back();
-            }
-            q.push_back(i);
-            while (q.front() == i - k) {
-                q.pop_front();
-            }
-            ans.push_back(nums[q.front()]);
-        }
-        return ans;
-    }
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+vector<int> postorderTraversal1(TreeNode *root);
+vector<int> postorderTraversal2(TreeNode *root);
 
 
-int main() {
-    vector<int> nums={1,3,-1,-3,5,3,6,7};
-    int k=3;
-    Solution solution;
-    vector<int> result = solution.maxSlidingWindow(nums,k);
-    for (int i = 0; i <result.size() ; ++i) {
-        cout<< result[i];
+// We have two methods here.
+//   1) the first one acutally is pre-order but reversed the access order.
+//   2) the second one is the traditional one
+
+vector<int> postorderTraversal(TreeNode *root) {
+    if (rand()%2){
+        cout << "---method one---" << endl;
+        return postorderTraversal1(root);
     }
+    cout << "---method two---" << endl;
+    return postorderTraversal2(root);
+}
 
+
+vector<int> postorderTraversal1(TreeNode *root) {
+    vector<int> v;
+    vector<TreeNode*> stack;
+    if (root) {
+        stack.push_back(root);
+    }
+    while (stack.size()>0){
+        TreeNode *n = stack.back();
+        stack.pop_back();
+        v.push_back(n->val);
+        if (n->left){
+            stack.push_back(n->left);
+        }
+        if (n->right) {
+            stack.push_back(n->right);
+        }
+    }
+    std::reverse(v.begin(), v.end());  // the trick
+    return v;
+}
+
+// traditional and standard way.
+// using the stack to simulate the recursive function stack.
+vector<int> postorderTraversal2(TreeNode *root) {
+    vector<int> v;
+    vector<TreeNode*> stack;
+    TreeNode *node = root;
+    TreeNode *lastVisitNode = NULL;
+    while(stack.size()>0 || node!=NULL){
+
+        if (node != NULL){
+            // keep going the left
+            stack.push_back(node);
+            node = node->left;
+        }else{
+            TreeNode *n = stack.back();
+            // left way is finsised, keep going to the right way
+            if (n->right != NULL && lastVisitNode != n->right){
+                node = n->right;
+            }else{
+                // both left and right has been accessed.
+                stack.pop_back();
+                v.push_back(n->val);
+                lastVisitNode = n;
+            }
+        }
+    }
+    return v;
+}
+
+
+TreeNode* createTree(int a[], int n)
+{
+    if (n<=0) return NULL;
+
+    TreeNode **tree = new TreeNode*[n];
+
+    for(int i=0; i<n; i++) {
+        if (a[i]==0 ){
+            tree[i] = NULL;
+            continue;
+        }
+        tree[i] = new TreeNode(a[i]);
+    }
+    int pos=1;
+    for(int i=0; i<n && pos<n; i++) {
+        if (tree[i]){
+            tree[i]->left = tree[pos++];
+            if (pos<n){
+                tree[i]->right = tree[pos++];
+            }
+        }
+    }
+    return tree[0];
+}
+
+void printTree_post_order(TreeNode *root)
+{
+    if (root == NULL){
+        //cout << "# ";
+        return ;
+    }
+    printTree_post_order(root->left);
+    printTree_post_order(root->right);
+    cout << root->val << " ";
+}
+
+
+void printArray(vector<int> v)
+{
+    for(int i=0; i<v.size(); i++){
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    srand(time(0));
+    int a[] = {1,2,3,4,5,0,6,0,0,7,8,9,0};
+    TreeNode* p = createTree(a, sizeof(a)/sizeof(int));
+    printTree_post_order(p);
+    cout << endl;
+    vector<int> v = postorderTraversal(p);
+    printArray(v);
+    cout << endl;
 
     return 0;
 }
