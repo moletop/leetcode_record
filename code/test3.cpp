7cc64479889e0663c8d23b240572a53d79459dba
molotop <liuyixd@163.com>
2021-08-27 22:51:53 +0800

    27th August

--- code/test3.cpp
+++ code/test3.cpp
@@ -1,30 +1,36 @@
-#include<iostream>
+#include <iostream>
+#include <queue>
 #include<cstring>
-#include<algorithm>
+#include <algorithm>
+#include<cmath>
+#include<set>
 using namespace std;
 
-struct student{
-    int grade;
-    char name[20];
-    int age;
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        set<string> a;
+        for (auto& i : nums) {
+            a.emplace(i);
+        }
+        int n = nums[0].size();
+        for (int i = 0; i < (1 << n); ++i) {
+            string x(n, '0');
+            for (int j = 0; j < n; ++j) {
+                x[j] = (1 & (i >> j)) + '0';
+            }
+            if (a.find(x) == a.end()) {
+                return x;
+            }
+        }
+        return string(n, '0');
+    }
 };
-
-bool cmp(student a,student b){
-    int t = strcmp(a.name,b.name);
-    if(a.grade != b.grade)
-        return a.grade > b.grade;
-    else if(t != 0)
-        return t > 0;
-    else
-        return a.age > b.age;
-}
-
 int main(){
-    student stu[5]={{98,"frank",15},{96,"frtgg",15},{78,"asfffas",13},{87,"sifhsh",17},{87,"sifhsh",16}};
-    sort(stu,stu+5,cmp);
-    for (int i = 0; i < 5;i++){
-        cout<<stu[i].grade<<" "<<stu[i].name<<" "<<stu[i].age<<" "<<endl;
-   }
-    return 0;
+    Solution solution;
+    vector<string>vec ={"101","000","111"};
+    string s=solution.findDifferentBinaryString(vec);
+    cout<<s<<endl;
 
+    return 0;
 }
