77b6236c8376961ddc2510040f7b690e169a9200
molotop <liuyixd@163.com>
2021-08-27 22:46:00 +0800

    27th August

---
+++ code/test4.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+ using namespace std;
+void test_2_wei_bag_problem1() {
+    vector<int> weight = {1, 3, 4};
+    vector<int> value = {15, 20, 30};
+    int bagWeight = 4;
+
+    // 二维数组
+    vector<vector<int>> dp(weight.size() , vector<int>(bagWeight + 1, 0));
+
+    // 初始化
+    for (int j = weight[0]; j <= bagWeight; j++) {
+        dp[0][j] = value[0];
+    }
+
+    // weight数组的大小 就是物品个数
+    for(int i = 1; i < weight.size(); i++) { // 遍历物品
+        for(int j = 0; j <= bagWeight; j++) { // 遍历背包容量
+            if (j < weight[i]) dp[i][j] = dp[i - 1][j];
+            else dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weight[i]] + value[i]);
+
+        }
+    }
+
+    cout << dp[weight.size() - 1][bagWeight] << endl;
+}
+
+int main() {
+    test_2_wei_bag_problem1();
+}
