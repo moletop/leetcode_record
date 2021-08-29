c14cb81f3cfeeea54c004586634aacc17ac92770
molotop <liuyixd@163.com>
2021-08-29 23:35:44 +0800

    29th August

---
+++ code/8.greedy algorithm/452.Minimum_Number_of_Arrows_to_Burst_Balloons.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include<string>
+using namespace std;
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points){
+        if(points.size()==1) return 1;
+
+        sort(points.begin(),points.end());
+        int flag=1;
+        for (int i = 1; i < points.size(); ++i) {
+            if(points[i][0]>points[i-1][1]) flag++;
+            else{
+                points[i][1]=min(points[i-1][1],points[i][1]);
+            }
+        }
+        return flag;
+    }
+};
+
