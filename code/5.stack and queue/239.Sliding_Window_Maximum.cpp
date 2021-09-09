8470481cc9ea8aef3504417651b3524f1e32b14e
molotop <liuyixd@163.com>
2021-09-09 12:09:11 +0800

    9th sep

--- code/5.stack and queue/239.Sliding_Window_Maximum.cpp
+++ code/5.stack and queue/239.Sliding_Window_Maximum.cpp
@@ -4,6 +4,8 @@
 #include <deque>
 using namespace std;
 
+//经典双端队列
+
 class Solution{
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
@@ -12,23 +14,25 @@ public:
             while(!q.empty()&&nums[i]>=nums[q.back()]){
                 q.pop_back();
             }
-            q.push_back(i);
+            q.push_back(i);//队列中存储的是下标
         }
 
         vector<int> result={nums[q.front()]};
-        for (int i = k; i <nums.size() ; ++i) {
+        for (int i = k; i <nums.size() ; ++i) {//窗口移动
             while(!q.empty()&&nums[i]>=nums[q.back()]){
                 q.pop_back();
             }
             q.push_back(i);
-            while(q.front()<=i-k){
+
+            while(q.front()==i-k){//如果最大值是窗口前面的值，则弹出
                 q.pop_front();
             }
-            result.push_back(nums[q.front()]);
+            result.push_back(nums[q.front()]);//每次移动，都获取以一个最大值
         }
         return result;
     }
 };
+
 
 
 int main(){
