0143effd68083f99458875104133220d9c6a20da
molotop <liuyixd@163.com>
2021-11-30 16:33:37 +0800

    30th nov

---
+++ code/2.linked list/21.Merge_Two_Sorted_Lists.cpp
@@ -0,0 +1,36 @@
+#include<bit/stdc++.h>
+using namespace stc;
+
+//链表常用操作：虚节点，指针操作
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(-1);
+        ListNode* cur = dummy;
+
+        while (l1 != nullptr && l2 != nullptr) {
+            ListNode* pp = (l1->val < l2->val) ? l1 : l2;  //精辟写法，学习！
+            cur->next = pp;
+            cur = cur->next;
+            if(pp == l1) l1= l1 ->next;
+            else l2 = l2-> next;
+        }
+        cur->next = (l1 == nullptr) ? l2 : l1;//精辟写法，学习！
+
+        ListNode* ans = dummy->next;
+        delete dummy;
+        return ans;
+    }
+};
+
