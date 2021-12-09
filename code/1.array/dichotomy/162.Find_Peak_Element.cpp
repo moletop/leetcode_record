86a67b39c66ddb7cba050ea47142435969c6d3ea
molotop <liuyixd@163.com>
2021-12-09 22:09:31 +0800

    9th Dec

--- code/1.array/dichotomy/162.Find_Peak_Element.cpp
+++ code/1.array/dichotomy/162.Find_Peak_Element.cpp
@@ -20,7 +20,7 @@ public:
         int l = 0; int r = n - 1;
         for(; l < r ; ){
             int mid = l + (r - l)/2;
-            if(nums[mid] > nums[mid+1])  r = mid
+            if(nums[mid] > nums[mid+1])  r = mid;
             else l = mid + 1;
         }
         return l;
