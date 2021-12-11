988beb98be44e90785b9b1a4f3a3d756983b2162
molotop <liuyixd@163.com>
2021-12-11 15:28:26 +0800

    11th Dec

--- code/3.hash table/169.Majority_Element.cpp
+++ code/3.hash table/169.Majority_Element.cpp
@@ -1,14 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//典型用哈希优化复杂度
-
-class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         unordered_map<int, int> map;
         int major = 0; int count = 0;
         for(auto i : nums){
+//典型用哈希优化复杂度
+
+            class Solution {
             map[i]++;
             if(map[i]>count) {count = map[i]; major = i;}
         }
