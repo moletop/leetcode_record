f66ae9428141e9ab5d3acf90cd8388f08e307e84
molotop <liuyixd@163.com>
2021-12-16 19:03:45 +0800

    16th Dec

--- code/2.linked list/21.Merge_Two_Sorted_Lists.cpp
+++ code/2.linked list/21.Merge_Two_Sorted_Lists.cpp
@@ -1,18 +1,16 @@
-#include<bit/stdc++.h>
-using namespace stc;
+#include<bits/stdc++.h>
+using namespace std;
 
 //链表常用操作：虚节点，指针操作
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+ };
+
 
 
 class Solution {
