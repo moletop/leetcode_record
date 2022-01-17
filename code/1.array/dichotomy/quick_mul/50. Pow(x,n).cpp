0c2b5d09086ed6ef9c3259fd3fc7d71bb58c41d2
molotop <liuyixd@163.com>
2022-01-17 16:32:08 +0800

    17th Jan

---
+++ code/1.array/dichotomy/quick_mul/50. Pow(x,n).cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+解题思路
+快速幂原理： 分治法
+此处是递归解决
+
+*/
+
+
+
+class Solution {
+public:
+    double quickMul(double x, long long N) {
+        if (N == 0) {
+            return 1.0;
+        }
+        double y = quickMul(x, N / 2);
+        return N % 2 == 0 ? y * y : y * y * x;
+    }
+
+    double myPow(double x, int n) {
+        long long N = n;
+        return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+    }
+};
+
+
+
