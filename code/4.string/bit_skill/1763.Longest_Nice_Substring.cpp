1ccc78aa5b680895299cf0c676b3fd5a49db6c49
molotop <liuyixd@163.com>
2022-02-18 21:52:10 +0800

    18th feb

---
+++ code/4.string/bit_skill/1763.Longest_Nice_Substring.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*当一个字符串 s 包含的每一种字母的大写和小写形式 同时 出现在 s 中，就称这个字符串 s 是 美好 字符串
+给你一个字符串 s ，请你返回 s 最长的 美好子字符串 。如果有多个答案，请你返回 最早 出现的一个。如果不存在美好子字符串，请你返回一个空字符串。
+*/
+
+class Solution {
+public:
+    string longestNiceSubstring(string s) {
+        int n = s.size();
+        int maxPos = 0;//起始点
+        int maxLen = 0;//长度
+
+        for (int i = 0; i < n; ++i) {
+            int lower = 0;
+            int upper = 0;
+            for (int j = i; j < n; ++j) {
+                if (islower(s[j])) {
+                    lower |= 1 << (s[j] - 'a');
+                } else {
+                    upper |= 1 << (s[j] - 'A');
+                }
+                if (lower == upper && j - i + 1 > maxLen) {
+                    maxPos = i;
+                    maxLen = j - i + 1;
+                }
+            }
+        }
+        return s.substr(maxPos, maxLen);
+    }
+};
+
